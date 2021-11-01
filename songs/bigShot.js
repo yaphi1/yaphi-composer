31eb5ca448a0d060426e2b610daefc92856b9d40
yaphi1 <yaphi1@users.noreply.github.com>
2021-11-01 00:57:42 -0400

    Add second part to spamton song

--- songs/bigShot.js
+++ songs/bigShot.js
@@ -700,6 +700,747 @@ const bigShot = {
 		[
 			{
 				"notes": [
+					"Bb2",
+					"Bb3"
+				],
+				"durationInBeats": 1
+			},
+			{
+				"notes": [
+					"F3"
+				],
+				"durationInBeats": 10
+			}
+		],
+		[
+			{
+				"notes": [
+					"Bb2"
+				],
+				"durationInBeats": 1
+			}
+		],
+		[
+			{
+				"notes": [
+					"C#4",
+					"C#3",
+					"Bb2"
+				],
+				"durationInBeats": 1
+			},
+			{
+				"notes": [
+					"Bb4",
+					"F4"
+				],
+				"durationInBeats": 2
+			}
+		],
+		[
+			{
+				"notes": [
+					"Bb2",
+					"Bb3"
+				],
+				"durationInBeats": 1
+			}
+		],
+		[
+			{
+				"notes": [
+					"Bb2"
+				],
+				"durationInBeats": 1
+			}
+		],
+		[
+			{
+				"notes": [
+					"Bb2",
+					"F3"
+				],
+				"durationInBeats": 1
+			},
+			{
+				"notes": [
+					"Bb4",
+					"F4"
+				],
+				"durationInBeats": 2
+			}
+		],
+		[
+			{
+				"notes": [
+					"Bb2",
+					"G#3"
+				],
+				"durationInBeats": 1
+			}
+		],
+		[
+			{
+				"notes": [
+					"Bb2",
+					"F3"
+				],
+				"durationInBeats": 1
+			}
+		],
+		[
+			{
+				"notes": [
+					"Bb2",
+					"Bb3"
+				],
+				"durationInBeats": 1
+			},
+			{
+				"notes": [
+					"Bb4",
+					"F4"
+				],
+				"durationInBeats": 2
+			}
+		],
+		[
+			{
+				"notes": [
+					"Bb2"
+				],
+				"durationInBeats": 1
+			}
+		],
+		[
+			{
+				"notes": [
+					"Bb2",
+					"C#4",
+					"C#3"
+				],
+				"durationInBeats": 1
+			}
+		],
+		[
+			{
+				"notes": [
+					"Bb2",
+					"Bb3"
+				],
+				"durationInBeats": 1
+			},
+			{
+				"notes": [
+					"C#5",
+					"G#4"
+				],
+				"durationInBeats": 2
+			}
+		],
+		[
+			{
+				"notes": [
+					"Bb2"
+				],
+				"durationInBeats": 1
+			}
+		],
+		[
+			{
+				"notes": [
+					"Bb2",
+					"E4",
+					"F3"
+				],
+				"durationInBeats": 1
+			}
+		],
+		[
+			{
+				"notes": [
+					"Bb2",
+					"D#4",
+					"G#3"
+				],
+				"durationInBeats": 1
+			},
+			{
+				"notes": [
+					"D#5",
+					"Bb4"
+				],
+				"durationInBeats": 2
+			}
+		],
+		[
+			{
+				"notes": [
+					"Bb2",
+					"C#4",
+					"F3"
+				],
+				"durationInBeats": 1
+			}
+		],
+		[
+			{
+				"notes": [
+					"Bb2",
+					"Bb3"
+				],
+				"durationInBeats": 1
+			},
+			{
+				"notes": [
+					"E3"
+				],
+				"durationInBeats": 10
+			}
+		],
+		[
+			{
+				"notes": [
+					"Bb2"
+				],
+				"durationInBeats": 1
+			}
+		],
+		[
+			{
+				"notes": [
+					"Bb2",
+					"C#4"
+				],
+				"durationInBeats": 1
+			},
+			{
+				"notes": [
+					"Bb4",
+					"F4"
+				],
+				"durationInBeats": 2
+			}
+		],
+		[
+			{
+				"notes": [
+					"Bb2",
+					"Bb3"
+				],
+				"durationInBeats": 1
+			}
+		],
+		[
+			{
+				"notes": [
+					"Bb2"
+				],
+				"durationInBeats": 1
+			}
+		],
+		[
+			{
+				"notes": [
+					"Bb2",
+					"F3"
+				],
+				"durationInBeats": 1
+			},
+			{
+				"notes": [
+					"Bb4",
+					"F4"
+				],
+				"durationInBeats": 2
+			}
+		],
+		[
+			{
+				"notes": [
+					"Bb2",
+					"G#3"
+				],
+				"durationInBeats": 1
+			}
+		],
+		[
+			{
+				"notes": [
+					"Bb2",
+					"F3"
+				],
+				"durationInBeats": 1
+			}
+		],
+		[
+			{
+				"notes": [
+					"Bb2",
+					"Bb3"
+				],
+				"durationInBeats": 1
+			},
+			{
+				"notes": [
+					"E5",
+					"B4"
+				],
+				"durationInBeats": 2
+			}
+		],
+		[
+			{
+				"notes": [
+					"Bb2"
+				],
+				"durationInBeats": 1
+			}
+		],
+		[
+			{
+				"notes": [
+					"Bb2",
+					"C#4"
+				],
+				"durationInBeats": 1
+			}
+		],
+		[
+			{
+				"notes": [
+					"Bb2",
+					"Bb3"
+				],
+				"durationInBeats": 1
+			},
+			{
+				"notes": [
+					"C#5",
+					"G#4"
+				],
+				"durationInBeats": 2
+			}
+		],
+		[
+			{
+				"notes": [
+					"Bb2"
+				],
+				"durationInBeats": 1
+			}
+		],
+		[
+			{
+				"notes": [
+					"Bb2",
+					"E4"
+				],
+				"durationInBeats": 1
+			}
+		],
+		[
+			{
+				"notes": [
+					"Bb2",
+					"D#4"
+				],
+				"durationInBeats": 1
+			},
+			{
+				"notes": [
+					"D#5",
+					"Bb4"
+				],
+				"durationInBeats": 2
+			}
+		],
+		[
+			{
+				"notes": [
+					"Bb2",
+					"C#4"
+				],
+				"durationInBeats": 1
+			}
+		],
+		[
+			{
+				"notes": [
+					"Bb2",
+					"Bb3"
+				],
+				"durationInBeats": 1
+			},
+			{
+				"notes": [
+					"F3"
+				],
+				"durationInBeats": 10
+			}
+		],
+		[
+			{
+				"notes": [
+					"Bb2"
+				],
+				"durationInBeats": 1
+			}
+		],
+		[
+			{
+				"notes": [
+					"Bb2",
+					"C#4"
+				],
+				"durationInBeats": 1
+			},
+			{
+				"notes": [
+					"Bb4",
+					"F4"
+				],
+				"durationInBeats": 2
+			},
+			{
+				"notes": [],
+				"durationInBeats": 10
+			}
+		],
+		[
+			{
+				"notes": [
+					"Bb2",
+					"Bb3"
+				],
+				"durationInBeats": 1
+			}
+		],
+		[
+			{
+				"notes": [
+					"Bb2"
+				],
+				"durationInBeats": 1
+			}
+		],
+		[
+			{
+				"notes": [
+					"Bb2",
+					"F3"
+				],
+				"durationInBeats": 1
+			},
+			{
+				"notes": [
+					"Bb4",
+					"F4"
+				],
+				"durationInBeats": 2
+			}
+		],
+		[
+			{
+				"notes": [
+					"Bb2",
+					"G#3"
+				],
+				"durationInBeats": 1
+			}
+		],
+		[
+			{
+				"notes": [
+					"Bb2",
+					"F3"
+				],
+				"durationInBeats": 1
+			}
+		],
+		[
+			{
+				"notes": [
+					"Bb2",
+					"Bb3"
+				],
+				"durationInBeats": 1
+			},
+			{
+				"notes": [
+					"Bb4",
+					"F4"
+				],
+				"durationInBeats": 2
+			}
+		],
+		[
+			{
+				"notes": [
+					"Bb2"
+				],
+				"durationInBeats": 1
+			}
+		],
+		[
+			{
+				"notes": [
+					"Bb2",
+					"C#4"
+				],
+				"durationInBeats": 1
+			}
+		],
+		[
+			{
+				"notes": [
+					"Bb2",
+					"Bb3"
+				],
+				"durationInBeats": 1
+			},
+			{
+				"notes": [
+					"C#5",
+					"G#4"
+				],
+				"durationInBeats": 2
+			}
+		],
+		[
+			{
+				"notes": [
+					"Bb2"
+				],
+				"durationInBeats": 1
+			}
+		],
+		[
+			{
+				"notes": [
+					"Bb2",
+					"E4"
+				],
+				"durationInBeats": 1
+			}
+		],
+		[
+			{
+				"notes": [
+					"Bb2",
+					"D#4"
+				],
+				"durationInBeats": 1
+			},
+			{
+				"notes": [
+					"D#5",
+					"Bb4"
+				],
+				"durationInBeats": 2
+			}
+		],
+		[
+			{
+				"notes": [
+					"Bb2",
+					"C#4"
+				],
+				"durationInBeats": 1
+			},
+			{
+				"notes": [],
+				"durationInBeats": 10
+			}
+		],
+		[
+			{
+				"notes": [
+					"Bb2",
+					"Bb3"
+				],
+				"durationInBeats": 1
+			},
+			{
+				"notes": [
+					"E3"
+				],
+				"durationInBeats": 10
+			}
+		],
+		[
+			{
+				"notes": [
+					"Bb2"
+				],
+				"durationInBeats": 1
+			},
+			{
+				"notes": [],
+				"durationInBeats": 10
+			}
+		],
+		[
+			{
+				"notes": [
+					"Bb2",
+					"C#4"
+				],
+				"durationInBeats": 1
+			},
+			{
+				"notes": [
+					"Bb4",
+					"F4"
+				],
+				"durationInBeats": 2
+			}
+		],
+		[
+			{
+				"notes": [
+					"Bb2",
+					"Bb3"
+				],
+				"durationInBeats": 1
+			}
+		],
+		[
+			{
+				"notes": [
+					"Bb2"
+				],
+				"durationInBeats": 1
+			}
+		],
+		[
+			{
+				"notes": [
+					"Bb2",
+					"F3"
+				],
+				"durationInBeats": 1
+			},
+			{
+				"notes": [
+					"Bb4",
+					"F4"
+				],
+				"durationInBeats": 2
+			}
+		],
+		[
+			{
+				"notes": [
+					"Bb2",
+					"G#3"
+				],
+				"durationInBeats": 1
+			}
+		],
+		[
+			{
+				"notes": [
+					"Bb2",
+					"F3"
+				],
+				"durationInBeats": 1
+			}
+		],
+		[
+			{
+				"notes": [
+					"Bb2",
+					"Bb3"
+				],
+				"durationInBeats": 1
+			},
+			{
+				"notes": [
+					"G#4",
+					"D#4"
+				],
+				"durationInBeats": 2
+			}
+		],
+		[
+			{
+				"notes": [
+					"Bb2"
+				],
+				"durationInBeats": 1
+			}
+		],
+		[
+			{
+				"notes": [
+					"Bb2",
+					"C#4"
+				],
+				"durationInBeats": 1
+			}
+		],
+		[
+			{
+				"notes": [
+					"Bb2",
+					"Bb3"
+				],
+				"durationInBeats": 1
+			},
+			{
+				"notes": [
+					"A4",
+					"E4"
+				],
+				"durationInBeats": 2
+			}
+		],
+		[
+			{
+				"notes": [
+					"Bb2"
+				],
+				"durationInBeats": 1
+			}
+		],
+		[
+			{
+				"notes": [
+					"Bb2",
+					"E4"
+				],
+				"durationInBeats": 1
+			}
+		],
+		[
+			{
+				"notes": [
+					"Bb2",
+					"D#4"
+				],
+				"durationInBeats": 1
+			},
+			{
+				"notes": [
+					"Bb4",
+					"F4"
+				],
+				"durationInBeats": 2
+			}
+		],
+		[
+			{
+				"notes": [
+					"Bb2",
+					"C#4"
+				],
+				"durationInBeats": 1
+			}
+		],
+		[
+			{
+				"notes": [
 					"Bb2"
 				],
 				"durationInBeats": 1
